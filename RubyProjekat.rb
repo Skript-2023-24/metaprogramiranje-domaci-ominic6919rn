4a9f154c72218fcdddbc24b9fd522ed3eecdc9ad
ominic6919rn <ominic6919rn@raf.rs>
2023-11-16 23:01:24 +0100

    Implementacija vise metoda

--- RubyProjekat.rb
+++ RubyProjekat.rb
@@ -1,6 +1,172 @@
 require "google_drive"
-session = GoogleDrive::Session.from_config("config.json")
 
-session.files.each do |file|
-  p file.title
+class SpreadSheet
+  include Enumerable
+
+  def initialize(ws)
+    rows = ws.rows
+    x = 0
+    y = 0
+    start = false
+
+    rows.each do |row|
+      break if start
+      row.each do |cell|
+        if cell != "" then
+          x = rows.index(row)
+          y = row.index(cell)
+          start = true
+          break
+        end
+      end
+    end
+
+    return nil if !start
+    @table = []
+    rows = rows.drop(x)
+    z = -1
+
+    rows[0].each do |cell|
+      if cell != "" then
+        z = rows[0].index(cell)
+      end
+    end
+
+    @table.push(rows[0][y..z])
+
+    rows[1..-1].each do |row|
+      empty = true
+      keyWord = false
+
+      row.each do |cell|
+        if cell != "" then
+          #delimo string po svim karakterima osim slovima, jer zelimo da nadjemo reci total ili subtotal u bilo kom obliku
+          #ali da ne budu deo neke druge pravilne reci
+          #prvo sam uradio sa =~ ali sam onda shvatio da mozda postoje reci koje sadrze total (osim subtotal)
+          words = cell.split(/[^a-zA-Z]+/)
+          words.each do |word|
+            if word.casecmp("total") || word.casecmp("subtotal") == 0
+              keyWord = true
+              break
+            end
+          end
+          if keyWord
+            break
+          end
+          empty = false
+        end
+      end
+
+      if !empty && !keyWord
+        @table.push(row[y..z])
+      end
+    end
+  end
+
+  def to_s
+    @table.each {|row| puts row }
+  end
+
+  def table
+    @table
+  end
+
+  def row(index)
+    @table[index]
+  end
+
+  def each
+    @table.each do |row|
+      row.each {|cell| yield cell}
+    end
+  end
+
+  def [](columnName)
+    columnIndex = -1
+
+    @table[0].each do |cell|
+      if cell == columnName
+        columnIndex = @table[0].index(cell)
+        break
+      end
+    end
+
+    return nil if columnIndex == -1
+
+    values = []
+
+    @table.each do |row|
+      values.push(row[columnIndex])
+    end
+
+    return values
+  end
+
+  def []=(key, value)
+    #p @table[0][key][@table[1]]
+  end
+
+  def +(sheet)
+    return nil if !sheet.instance_of? self.class || sheet.row(0) != @table[0]
+    @table + sheet.table.drop(1)
+  end
+
+  #sto se ovog dela tice postoji naravno problem/greska usled mergovanih celija (na primer: red u prvoj tabeli koji sadrzi
+  #mergovano celiju koja nije prva ce ustvari biti prazna celija i poklopice se sa redom u drugoj tabeli koji na tom mestu ima praznu
+  #celiju)
+  #slozenost n * m (sto je realno uzas ali razumeo sam sto se tice ovog projekta da je nebitno, a ovo mi je naravno
+  #najlakse, msm sta ce biti lakse nego dupla petlja za pretragu)...ako me kosta poena zamolio bih vas da mi kazete na odbrani,
+  #odmah cu pred vama smisliti bolje (bas sam dobar u algoritmima :))
+  def -(sheet)
+    return nil if !sheet.instance_of? self.class || sheet.row(0) != @table[0]
+    table = []
+    table.push(@table[0])
+
+    @table[1..-1].each do |row|
+      same = false
+      sheet.table[1..-1].each do |row1|
+        if row[0] == row1[0]
+          same = true
+          for index in 1..row.size - 1
+            if row[index] != row1[index]
+              same = false
+              break
+            end
+          end
+          if same
+            break
+          end
+        end
+      end
+      if !same
+        table.push(row)
+      end
+    end
+    return table
+  end
 end
+
+def main
+  session = GoogleDrive::Session.from_config("config.json")
+
+  sheet = SpreadSheet.new(session.spreadsheet_by_key("1ThNbxm3uobmwIbTb1V5u8AqH_SkY31kdTMQmEqNrvtA").worksheets[0])
+  sheet1 = SpreadSheet.new(session.spreadsheet_by_key("16FeOczt_Kjsuuubuk7MktyjIGZcSwFrUUPOzhr03mlY").worksheets[0])
+
+  p sheet.table
+
+  p sheet.row(1)
+
+  sheet.each do |cell|
+    p cell
+  end
+
+  p sheet["Prva Kolona"]
+
+  p sheet["Prva Kolona"][1]
+
+  p sheet + sheet1
+
+  p sheet - sheet1
+end
+
+main
